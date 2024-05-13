3eae9d2c6769b996623cd17c8bb1aeba0087aced
Leomar Estrada <leomarestradabsas@gmail.com>
2024-05-13 11:50:06 -0300

    update

--- main.py
+++ main.py
@@ -78,11 +78,11 @@ if selected == "Bajar videos":
       
    st.title("Video Downloader")
 
-youtube_url = st.text_input("Youtube URL:",placeholder="Enter here...")
-st.button("Get Video", on_click=download_video,args=(youtube_url,0))
+   youtube_url = st.text_input("Youtube URL:",placeholder="Enter here...")
+   st.button("Get Video", on_click=download_video,args=(youtube_url,0))
 
-if 'video_downloaded' in st.session_state:
-    st.download_button(label="Video Download",data=st.session_state['video_bytes'],file_name=st.session_state['video_filename'],mime='application/octet-stream')
+   if 'video_downloaded' in st.session_state:
+      st.download_button(label="Video Download",data=st.session_state['video_bytes'],file_name=st.session_state['video_filename'],mime='application/octet-stream')
 
 
 ##############################################################################################
